4ecf84126ab5ff284bde805ee689ced5a841e3c0
Cristiano Viana <cristiano.gvianati@gmail.com>
2018-06-26 20:16:14 -0300

    Banco dedados H2 criação do banco automático

--- src/main/java/com/jrcg/curso/domain/Categoria.java
+++ src/main/java/com/jrcg/curso/domain/Categoria.java
@@ -2,11 +2,22 @@ package com.jrcg.curso.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	
+	@Id()
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column
 	private String nome;
 
 	public Categoria() {
